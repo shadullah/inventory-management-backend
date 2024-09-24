e730022102fd0e648bec5dd32762fa7dce5b3758
hexaalif <shadullahsad9@gmail.com>
2024-09-24 13:09:45 +0600

    pagination implemented

--- products/views.py
+++ products/views.py
@@ -1,12 +1,22 @@
 from rest_framework import viewsets
-from rest_framework import filters
+from rest_framework import filters,pagination
 
 from .serializers import ProductSerializer
 from .models import products
 
 # Create your views here.
+
+
+class productPagination(pagination.PageNumberPagination):
+    page_size=4 # number of items
+    page_size_query_param=page_size
+    max_page_size=100
+
+
+# product viewsets here
 class ProductView(viewsets.ModelViewSet):
-    queryset = products.objects.all()
+    queryset = products.objects.all().order_by("id")
     serializer_class = ProductSerializer
     filter_backends = [filters.SearchFilter]
     search_fields = ['name']
+    pagination_class = productPagination
